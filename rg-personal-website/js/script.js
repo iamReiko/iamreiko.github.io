d083288f62c3c87d0e7dc8887dc29467c6b2d5df
iamReiko <79856341+iamReiko@users.noreply.github.com>
2021-08-01 22:11:55 +0800

    Delete script.js

--- rg-personal-website/js/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("menuicon").onclick = function() {togglePanel()};
-
-function togglePanel() {
-    document.getElementById("nav").classList.toggle("show");
-}
-
-/*----------------------------------------------------*/
-/* Highlight the current section in the navigation bar
-------------------------------------------------------*/
